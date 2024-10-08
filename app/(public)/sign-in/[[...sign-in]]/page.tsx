515b88bfd7dd4f90b5650a0fcda41016ae06f0e5
My Little Pet <mylittlepetinternal@gmail.com>
2024-10-08 11:17:27 -0300

    arrumei o signout e bug no middleware

--- app/(public)/sign-in/[[...sign-in]]/page.tsx
+++ app/(public)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
   return (
     <main className='flex-1 flex items-center justify-center container min-h-screen'>
-      <SignIn />
+      <SignIn fallbackRedirectUrl="/dashboard"/>
     </main>
   )
 }
